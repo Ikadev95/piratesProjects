3a0587c69dcb321ecfc0d0f4360a0584659332d2
andreaDevelope <andrea_develope@outlook.it>
2024-10-29 18:00:50 +0100

    implements-caccia-al-tesoro

---
+++ src/app/pages/caccia-al-tesoro/caccia-al-tesoro.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-caccia-al-tesoro',
+  templateUrl: './caccia-al-tesoro.component.html',
+  styleUrl: './caccia-al-tesoro.component.scss'
+})
+export class CacciaAlTesoroComponent {
+
+}
