0219b321c9ea8d75fc954ad166014acf6b07a507
Ikadev95 <fegughi@gmail.com>
2024-10-29 17:03:42 +0100

    customize work in progress

--- src/app/pages/customize/customize.component.ts
+++ src/app/pages/customize/customize.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-customize',
   templateUrl: './customize.component.html',
   styleUrl: './customize.component.scss'
 })
-export class CustomizeComponent {
+export class CustomizeComponent implements OnInit{
 
   foto:string = "/pappagalli/init.png"
+
+  fotos: string[] = [
+    "/pappagalli/init.png",
+    "/pappagalli/bandana.png",
+    "/pappagalli/cintura.png",
+    "/pappagalli/cappellopirata.png",
+    "/pappagalli/spada.png",
+    "/pappagalli/spada-bandana.png",
+    "/pappagalli/cintura-spada.png",
+    "/pappagalli/cintura-bandana.png",
+    "/pappagalli/spada-bandana.png",
+    "/pappagalli/spada-cintura-bandana.png",
+    "/pappagalli/spada-cappellopirata-cintura.png",
+    "/pappagalli/cappellopirata-cintura.png",
+    "/pappagalli/cappellopirata.png"
+  ]
+
+
+  ngOnInit(): void {
+    console.log(this.fotos.filter(foto => foto.includes("cappello")))
+    console.log(this.fotos.filter(foto => foto === "cappellopirata"))
+  }
+
 
 }
