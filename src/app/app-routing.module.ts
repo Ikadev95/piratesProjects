507d5f6d59cb24cd534a5d54059ee45250a3d21e
Federico Peralta <federico.peralta91@gmail.com>
2024-10-29 17:15:54 +0100

    rotta 404

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { UserGuard } from './guards/user.guard';
 
 const routes: Routes = [
-
+  {
+    path: '',
+    loadChildren: () =>
+      import('./pages/auth/auth.module').then((m) => m.AuthModule),
+    canActivate: [UserGuard],
+  },
   {
     path: 'home',
     loadChildren: () =>
@@ -26,12 +31,18 @@ const routes: Routes = [
     canActivate: [UserGuard],
   },
   {
-    path: '',
+    path: 'login',
     loadChildren: () =>
       import('./pages/log-in/log-in.module').then((m) => m.LogInModule),
     canActivate: [UserGuard],
   },
-  { path: 'customize', loadChildren: () => import('./pages/customize/customize.module').then(m => m.CustomizeModule) },
+  {
+    path: 'customize',
+    loadChildren: () =>
+      import('./pages/customize/customize.module').then(
+        (m) => m.CustomizeModule
+      ),
+  },
   {
     path: '**',
     loadChildren: () =>
