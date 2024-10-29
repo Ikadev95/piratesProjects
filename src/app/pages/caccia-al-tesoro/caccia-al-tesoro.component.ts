9a5b65dd3b09fd73160cb74b21b3955504c43a40
andreaDevelope <andrea_develope@outlook.it>
2024-10-29 21:30:58 +0100

    start-function-caccia-al-tesoro

--- src/app/pages/caccia-al-tesoro/caccia-al-tesoro.component.ts
+++ src/app/pages/caccia-al-tesoro/caccia-al-tesoro.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-caccia-al-tesoro',
   templateUrl: './caccia-al-tesoro.component.html',
-  styleUrl: './caccia-al-tesoro.component.scss'
+  styleUrl: './caccia-al-tesoro.component.scss',
 })
-export class CacciaAlTesoroComponent {
-
-}
+export class CacciaAlTesoroComponent {}
